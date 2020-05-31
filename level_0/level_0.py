ff62a560c724d1ce6fc202844da3c532b451f9d6
Boul3ez85 <benrebah.moez@gmail.com>
2020-05-31 23:07:28 +0100

    vote 1024 times

--- level_0/level_0.py
+++ level_0/level_0.py
@@ -5,10 +5,9 @@ URL = "http://158.69.76.135/level0.php"
 
 payload = {"id": "1406", "holdthedoor": "Submit+Query"}
 
-for i in range(1024):
+for a in range(1024):
     response = requests.post(URL, data=payload)
     if response.status_code == 200:
-        print("{:d} The request has succeeded.".format(i))
+        print("{:d} The request has succeeded.".format(a))
     else:
-        print("{:d} The request has fail.".format(i))
-        break
+        print("{:d} The request has failed.".format(a))
