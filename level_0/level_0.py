4061ad2639838f3597bcf73aac58e7742828d6f2
Boul3ez85 <benrebah.moez@gmail.com>
2020-05-31 22:37:18 +0100

    vote 1024 times

---
+++ level_0/level_0.py
@@ -0,0 +1,14 @@
+import requests
+
+
+URL = "http://158.69.76.135/level0.php"
+
+payload = {"id": "1406", "holdthedoor": "Submit+Query"}
+
+for i in range(1024):
+    response = requests.post(URL, data=payload)
+    if response.status_code == 200:
+        print("{:d} The request has succeeded.".format(i))
+    else:
+        print("{:d} The request has fail.".format(i))
+        break
